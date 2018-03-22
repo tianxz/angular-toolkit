a07df058e6ac9e6339fcdad3572aca0f7e9f71c0
XizeTian <oc.mt@hotmail.com>
2018-03-22 17:36:38 +0800

    ..

--- misc/demo/scripts/number-demo.js
+++ misc/demo/scripts/number-demo.js
@@ -4,7 +4,6 @@
     angular
         .module('at.number.demo', [ 'angular.toolkit.number', 'ui.bootstrap' ])
         .controller('atNumberDemoController', function ($scope) {
-            $scope.obj = 1234;
             $scope.placeholder = '请输入整数或浮点数';
         })
 })(window);
